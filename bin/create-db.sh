069e3601f4e2271ab2e9123d59fef296b2d2fd06
Bobjoy <bobjoy1010@qq.com>
2017-04-21 18:21:27 +0800

    :construction_worker:添加maven-db-plugin

--- bin/create-db.sh
+++ bin/create-db.sh
@@ -2,5 +2,10 @@
 echo [INFO] create db.
 echo [INFO] please confirm db encoding utf-8.
 echo [INFO] please confirm has modified es/web/pom.xml
+
+cd ./plugins/maven-db-plugin
+mvn clean install -pl . -Dmaven.test.skip=true
+cd ../../
+
 cd ./web
 mvn db:create
--- bin/install.sh
+++ bin/install.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 echo [INFO] Install es.
-cd ..
 mvn clean install -pl . -Dmaven.test.skip=true
 
 echo [INFO] Install es/parent.
 cd ./parent
 mvn clean install -pl . -Dmaven.test.skip=true
-
+cd ../
 
 echo [INFO] Install es/common.
 cd ./common
