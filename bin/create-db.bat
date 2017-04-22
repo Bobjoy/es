c79c39c6c53ec32e9113aae7fbdf284c904e987e
bobjoy <bobjoy1010@163.com>
2017-04-22 08:39:48 +0800

    修改CI数据库密码

--- bin/create-db.bat
+++ bin/create-db.bat
@@ -3,7 +3,11 @@ echo [INFO] create db.
 echo [INFO] please confirm db encoding utf-8.
 echo [INFO] please confirm has modified es/web/pom.xml--^>profile--^>development---^>connection.admin.url/connection.username/password 
 cd %~dp0
-cd ../web
+
+cd ../plugins/maven-db-plugin
+call mvn clean install -pl . -Dmaven.test.skip=true
+
+cd ../../web
 call mvn db:create
 
 cd ../bin
